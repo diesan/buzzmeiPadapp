2cd41f21d0685c52591e701b1b80449d28a78c2f
Diego Sanchez <dsanchez@clunda.co>
2015-05-02 16:33:32 -0400

    Initial Commit

---
+++ buzzMe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  buzzMe
+//
+//  Created by Diego Sanchez on 5/2/15.
+//  Copyright (c) 2015 BuzzMe Team. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ buzzMe/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  buzzMe
+//
+//  Created by Diego Sanchez on 5/2/15.
+//  Copyright (c) 2015 BuzzMe Team. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
